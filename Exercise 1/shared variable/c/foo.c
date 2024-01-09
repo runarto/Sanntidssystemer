965b933dde9dec6c315b5f48882a1445a702fc8b
Runar Tobiassen <114917472+runarto@users.noreply.github.com>
2024-01-09 17:18:03 +0100

    First task complete

--- Exercise 1/shared variable/c/foo.c
+++ Exercise 1/shared variable/c/foo.c
@@ -9,23 +9,60 @@ int i = 0;
 // Note the return type: void*
 void* incrementingThreadFunction(){
     // TODO: increment i 1_000_000 times
+    int *p = &i;
+    for (int j = 0; j < 1000000; j++)
+    {
+        (*p)++; 
+    }
     return NULL;
 }
 
 void* decrementingThreadFunction(){
     // TODO: decrement i 1_000_000 times
+
+    int *p = &i; 
+    for (int j = 0; j < 1000000; j++)
+    {
+        (*p)--; 
+    }
+
     return NULL;
 }
 
 
 int main(){
+    pthread_t increment;
+    pthread_t decrement;
+    
+
     // TODO: 
     // start the two functions as their own threads using `pthread_create`
     // Hint: search the web! Maybe try "pthread_create example"?
     
     // TODO:
     // wait for the two threads to be done before printing the final result
-    // Hint: Use `pthread_join`    
+    // Hint: Use `pthread_join`   
+
+    int incrementResult = pthread_create(&increment, NULL, incrementingThreadFunction, NULL);
+    int decrementResult = pthread_create(&decrement, NULL, incrementingThreadFunction, NULL);
+
+    if (incrementResult == 0) {
+        // Successfully created the thread, now wait for it to finish
+        pthread_join(increment, NULL);
+    } else {
+        // An error occurred
+        fprintf(stderr, "Error: pthread_create returned %d\n", incrementResult);
+    }
+
+    if (decrementResult == 0) {
+        // Successfully created the thread, now wait for it to finish
+        pthread_join(decrement, NULL);
+    } else {
+        // An error occurred
+        fprintf(stderr, "Error: pthread_create returned %d\n", decrementResult);
+    }
+
+    // Your main program continues here
     
     printf("The magic number is: %d\n", i);
     return 0;
